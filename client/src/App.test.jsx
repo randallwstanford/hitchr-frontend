3409c8de127156aab955c42b5744e1918eee62ed
Sam Pool <sambpool@gmail.com>
2021-10-15 20:17:51 -0600

    add testing dependencies

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -1,5 +1,10 @@
 /* eslint-disable no-undef */
+import React from 'react';
+import { render } from '@testing-library/react';
+
+// components
+import App from './App';
 
 it('should render', () => {
-
+  render(<App />);
 });
