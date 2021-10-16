5bf7fc3eff7bd899372b156b86e40a4ecb2f2edf
randallwstanford <randallwstanford@gmail.com>
2021-10-16 11:21:10 -0600

    Adds postMessage and moves getMessages.

--- client/src/serverUtils.js
+++ client/src/serverUtils.js
@@ -5,7 +5,7 @@ const baseUrl = 'http://localhost:3000/api';
 export default {
   ride: {
     postRide: (formData) => new Promise((resolve, reject) => {
-      axios.post(`${baseUrl}/ride`, formData)
+      axios.post(`${baseUrl}/ride\`${formData}`)
         .then(({ data }) => resolve(data))
         .catch(reject);
     }),
@@ -18,6 +18,12 @@ export default {
     }),
     getMessages: (userId, recipientId) => new Promise((resolve, reject) => {
       axios.get(`${baseUrl}/messages/${userId}/?recipient=${recipientId}`)
+        .then(({ data }) => resolve(data))
+        .catch(reject);
+    }),
+    postMessage: (formData) => new Promise((resolve, reject) => {
+      const { userId, recipientId } = formData; // ?
+      axios.post(`${baseUrl}/messages/${userId}?recipient=${recipientId}`, formData)
         .then(({ data }) => resolve(data))
         .catch(reject);
     }),
