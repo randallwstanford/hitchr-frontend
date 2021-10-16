fa0905998ce2330b795594e052acda55bfc91143
randallwstanford <randallwstanford@gmail.com>
2021-10-16 11:37:30 -0600

    Deletes newRide

--- client/src/views/NewRide/NewRide.test.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import NewRide from './NewRide';
-
-it('should render without crashing', () => {
-  render(<NewRide />);
-});
