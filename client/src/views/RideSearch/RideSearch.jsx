4fd9f6889f0fd2266f19e76aa8ec2774c941f182
Sam Pool <sambpool@gmail.com>
2021-10-15 18:18:01 -0600

    write RideSearch skeleton

---
+++ client/src/views/RideSearch/RideSearch.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Stylesheet
+import './RideSearch.css';
+
+const RideSearch = () => (
+  <div>
+    <form>
+      <input id="input-start-loc" />
+      <input id="input-start-loc" />
+      <button type="submit">Search Rides</button>
+    </form>
+    <div id="search-results">
+      { /* TODO: Add search results sub-component */ }
+    </div>
+  </div>
+);
+export default RideSearch;
