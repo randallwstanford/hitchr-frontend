6cbc526492b8e80c00c6909e8c486a8978914416
randallwstanford <randallwstanford@gmail.com>
2021-10-15 20:14:43 -0600

    Adds Dashboard component and subcomponents for the user dashboard.

--- client/src/views/Dashboard/Dashboard.jsx
+++ client/src/views/Dashboard/Dashboard.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import UpcomingTrips from './UpcomingTrips/UpcomingTrips';
+import PastTrips from './PastTrips/PastTrips';
 
 const Dashboard = () => (
   <div>
-    Dashboard
+    <h1>Hitchr</h1>
+    -username-
+    -payment option 1-
+    -payment option 2-
+    <UpcomingTrips />
+    <PastTrips />
   </div>
 );
 
---
+++ client/src/views/Dashboard/PastTrips/PastTrips.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PastTrips = () => (
+  <div>
+    {/* Mapped Elements */}
+    <span>Start</span>
+    <span>End</span>
+    <span>User</span>
+    {/* Mapped Elements */}
+  </div>
+);
+
+export default PastTrips;
