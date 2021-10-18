831dd2b1497569f9959cda44b4489eeff8f00748
Sam Pool <sambpool@gmail.com>
2021-10-18 17:07:25 -0600

    add UserContext for test rendering

--- client/src/components/Nav/Nav.test.jsx
+++ client/src/components/Nav/Nav.test.jsx
@@ -4,7 +4,9 @@ import { render } from '@testing-library/react';
 
 // Components
 import Nav from './Nav';
+import UserContext from '../../contexts/UserContext';
+import userInfo from '../../dummyData/userInfo';
 
 it('should render without crashing', () => {
-  render(<Nav />);
+  render(<UserContext.Provider value={userInfo}><Nav /></UserContext.Provider>);
 });
