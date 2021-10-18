4234882002fc741d2ee928144e32735facbc1405
Sam Pool <sambpool@gmail.com>
2021-10-18 14:33:17 -0600

    implement user context

---
+++ client/src/dummyData/emptyUser.js
@@ -0,0 +1,8 @@
+const emptyUser = {
+  user_id: null,
+  username: null,
+  rating: null,
+  sessionId: '122gjsadlfkj8ldksajf',
+  payment_methods: [],
+};
+export default emptyUser;
---
+++ client/src/dummyData/userInfo.js
@@ -0,0 +1,20 @@
+export default {
+  user_id: 12345,
+  username: 'MrDriver',
+  rating: 4.5,
+  sessionId: '122gjsadlfkj8ldksajf',
+  payment_methods: [
+    {
+      provider: 'Venmo',
+      url: 'https://venmo.com/MrDriver',
+    },
+    {
+      provider: 'PayPal',
+      url: 'https://paypal.com/MrDriver',
+    },
+    {
+      provider: 'Cash App',
+      url: 'https://cashapp.com/MrDriver',
+    },
+  ],
+};
