c18f3dbd483c8361237e9af0c384338d43a981cd
Sam Pool <sambpool@gmail.com>
2021-10-18 12:39:03 -0600

    begin RideSearch funtionality

--- client/src/views/RideSearch/RideSearch.jsx
+++ client/src/views/RideSearch/RideSearch.jsx
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import RideList from '../../components/RideList/RideList';
+import serverUtils from '../../serverUtils';
 
 // Stylesheet
 // import './RideSearch.css';
 
 const RideSearch = () => {
   const [rideResults, setRideResults] = useState([]);
-  function fetchRides() {
+  function handleSearch() {
+    const start = document.getElementById('input-start-loc').value;
+    const end = document.getElementById('input-end-loc').value;
+    serverUtils.searchRide(start, end).then((results) => setRideResults(results));
     setRideResults([]);
   }
-  useEffect(() => {
-    fetchRides();
-  }, []);
   return (
     <div>
       <form>
@@ -19,11 +20,11 @@ const RideSearch = () => {
           Starting Location:
           <input id="input-start-loc" />
         </label>
-        <label htmlFor="input-start-loc">
+        <label htmlFor="input-end-loc">
           Ending Location:
-          <input id="input-start-loc" />
+          <input id="input-end-loc" />
         </label>
-        <button type="submit">Search Rides</button>
+        <button type="button" onClick={handleSearch}>Search Rides</button>
       </form>
       <div id="search-results">
         <RideList rides={rideResults} />
--- client/src/views/RideSearch/RideSearch.test.jsx
+++ client/src/views/RideSearch/RideSearch.test.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import RideSearch from './RideSearch';
 
-it('should render without crashing', () => {
-  render(<RideSearch />);
+// Mock Libraries
+jest.mock('axios'); // this happens automatically with automocking
+const axios = require('axios');
+
+// Mock Functions
+axios.post.mockImplementation((url, data) => ({ url, data }));
+axios.get.mockImplementation((url, data) => ({ url, data }));
+
+describe('RideSearch', () => {
+  it('should render without crashing', () => {
+    render(<RideSearch />);
+  });
 });
