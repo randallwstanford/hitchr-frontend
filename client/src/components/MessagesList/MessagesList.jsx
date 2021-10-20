d9c904475bb5703b5a75bdb6d90dd52258fedfe2
randallwstanford <randallwstanford@gmail.com>
2021-10-20 14:28:00 -0600

    adds function to handle submit.

--- client/src/components/MessagesList/MessagesList.jsx
+++ client/src/components/MessagesList/MessagesList.jsx
@@ -1,22 +1,54 @@
 // React
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+// import axios from 'axios';
 import PropTypes from 'prop-types';
+// import UserContext from '../../contexts/UserContext';
+import serverUtils from '../../serverUtils';
 
 // Stylesheet
 import './MessagesList.css';
 import Message from '../Message/Message';
 
-const MessagesList = ({ messages }) => (
-  <div data-testid="MessagesList" className="messages-container">
-    {
-      messages.length
-        ? messages.map((message) => <Message message={message} key={`message-${message.messageId}`} />)
-        : null
-    }
-  </div>
-);
+const MessagesList = ({ messages }) => {
+  const { userId } = useParams();
+  let recipientId = 0;
+
+  const [allMessages, setMessages] = useState([messages]);
+
+  useEffect(() => {
+    setMessages(messages);
+  }, []);
+  allMessages.forEach((message) => {
+    if (message.senderId !== userId) recipientId = message.senderId;
+  });
+
+  const newMessage = (e) => {
+    e.preventDefault();
+    const newMessageToPost = e.target.newMessage.value;
+    const formData = { text: newMessageToPost, senderId: parseInt(userId, 10), recipientId };
+    serverUtils.messages.postMessage(formData, parseInt(userId, 10), recipientId);
+  };
+
+  return (
+    <div data-testid="MessagesList" className="messages-container">
+      {
+        messages.length
+          ? allMessages.map((message) => <Message message={message} key={`message-${message.messageId}`} />)
+          : null
+      }
+      <form onSubmit={newMessage}>
+        <input name="newMessage" placeholder="Enter Message Here" />
+        <button type="button">Post Message</button>
+      </form>
+    </div>
+  );
+};
+
 export default MessagesList;
 
 MessagesList.propTypes = {
-  messages: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  // messages: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  messages: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
