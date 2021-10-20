efffbb25437d73fd265047e3cab7c1a2350e98a8
randallwstanford <randallwstanford@gmail.com>
2021-10-19 22:20:10 -0600

    Adds dynamic username, text, and formatted date from updated dummy data from query.

--- client/src/components/Message/Message.jsx
+++ client/src/components/Message/Message.jsx
@@ -5,14 +5,32 @@ import PropTypes from 'prop-types';
 // Stylesheet
 import './Message.css';
 
-const Message = ({ message }) => (
-  <span>{message.text}</span>
-);
+const Message = ({ message }) => {
+  const formatDate = (date) => new Date(date).toLocaleDateString('en-us', {
+    year: 'numeric', day: 'numeric', month: 'short', hour: '2-digit', minute: '2-digit',
+  });
+
+  return (
+    <div className="message">
+      { message.username }
+      :
+      &nbsp;
+      {message.text}
+      &nbsp;
+      Date:
+      &nbsp;
+      {formatDate(message.timestamp)}
+    </div>
+  );
+};
+
 export default Message;
 
 Message.propTypes = {
   message: PropTypes.shape({
     text: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    timestamp: PropTypes.string.isRequired,
     senderId: PropTypes.number.isRequired,
     recipientId: PropTypes.number.isRequired,
   }).isRequired,
