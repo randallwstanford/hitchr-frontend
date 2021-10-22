bd4c98568c58e8a400bf48f13ef5318cbb7d807c
Trevor Settles <roboteng@gmail.com>
2021-10-21 20:08:42 -0600

    remove console.log

--- client/src/views/Login/LoginModel.js
+++ client/src/views/Login/LoginModel.js
@@ -1,8 +1,7 @@
 import api from '../../serverUtils';
 
 const LoginUser = (username, password) => {
-  api.auth.login(username, password)
-    .then(console.log);
+  api.auth.login(username, password);
 };
 
 export default LoginUser;
