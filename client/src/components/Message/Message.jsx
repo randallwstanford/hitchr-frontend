151f23bf655f2afe3fd6bb8ff022696c42508444
randallwstanford <randallwstanford@gmail.com>
2021-10-21 23:10:02 -0600

    Adds conditional class rendering for less and right chat boxes.

--- client/src/components/Message/Message.jsx
+++ client/src/components/Message/Message.jsx
@@ -1,23 +1,29 @@
 // React
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-
+import UserContext from '../../contexts/UserContext';
 // Stylesheet
 import './Message.css';
 
 const Message = ({ message }) => {
+  const { username } = useContext(UserContext);
+
   const formatDate = (date) => new Date(date).toLocaleDateString('en-us', {
     year: 'numeric', day: 'numeric', month: 'short', hour: '2-digit', minute: '2-digit',
   });
 
+  const leftOrRight = `${(message.username === username) ? 'left' : 'right'}`;
+
   return (
     <div className="message">
-      { message.username }
-      :
-      <p data-testid="message-test" className="message-text">{message.text}</p>
-      <div>
-        Date: &nbsp;
-        {formatDate(message.timestamp)}
+      <div className={leftOrRight}>
+        { message.username }
+        :
+        <p data-testid="message-test" className="message-text">{message.text}</p>
+        <div>
+          Date: &nbsp;
+          {formatDate(message.timestamp)}
+        </div>
       </div>
     </div>
   );
